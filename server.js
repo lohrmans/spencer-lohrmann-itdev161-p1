26878e1ec120a3126d20d290bbe63ecba81ba003
Spencer Lohrmann <lohrmans@gmatc.matc.edu>
2021-10-11 08:24:37 -0500

    Added API Endpoint

--- server.js
+++ server.js
@@ -8,10 +8,18 @@ const app = express();
 //Connect database
 connectDatabase();
 
-//API endpoints
+//Configure Middleware
+app.use(express.json({ extended: false }));
+
+//API Endpoints	
 app.get('/', (req, res) =>
 	res.send('HTTP request sent to root API endpoint.')
 );
+
+app.post('/api/users', (request, res) => {
+	console.log(req.body);
+	res.send(req.body);
+});
 
 //Connection listener
 app.listen(3000, () => console.log('Express server running on port 3000.'));
