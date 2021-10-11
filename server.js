02dd60cc6608db574a6d0d6b98e0aa80df033870
Spencer Lohrmann <lohrmans@gmatc.matc.edu>
2021-10-11 10:58:09 -0500

    Added data validation

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
-
 import connectDatabase from './config/db';
+import { check, validationResult } from 'express-validator';
 
 //Initialize express application
 const app = express();
@@ -16,10 +16,23 @@ app.get('/', (req, res) =>
 	res.send('HTTP request sent to root API endpoint.')
 );
 
-app.post('/api/users', (request, res) => {
-	console.log(req.body);
-	res.send(req.body);
-});
+app.post(
+	'/api/users', 
+	[
+		check('name', 'Please enter your name').not().isEmpty(),
+		check('email', 'Please enter a valid email').isEmail(),
+		check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })	
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(422).json({ errors: errors.array() });
+		}
+		else {
+			return res.send(req.body);
+		}
+	}
+);
 
 //Connection listener
 app.listen(3000, () => console.log('Express server running on port 3000.'));
